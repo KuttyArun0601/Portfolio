e4a2a9f8ffff3fff8b16152772ea478a8d05368e
Arun-06 <90039797+KuttyArun0601@users.noreply.github.com>
2024-08-26 03:06:44 +0530

    navication updation for mobile resolution

--- func.js
+++ func.js
@@ -1,4 +1,36 @@
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navList2 = document.getElementById('nav-list2');
+    const listButton = document.getElementById('list');
+    const cancelButton = document.getElementById('cancel-btn');
+    const navLinks = document.querySelectorAll('.nav_link a');
+    
+
+
+    
+    listButton.addEventListener('click', function() {
+      navList2.style.display = 'block';
+      navList2.style ="padding: 10px 0px 10px 0px";
+      cancelButton.style.display="block";
+    });
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+          navList2.style.display = 'none';
+          cancelButton.style.display = 'none';
+        });
+      });
+
+    cancelButton.addEventListener('click', function() {
+      navList2.style.display = 'none';
+      cancelButton.style.display="none";
+    });
+  });
+ 
+
+
+
 document.getElementById('contact-info').addEventListener('submit',function(event){
     event.preventDefault();
     var data = new FormData(event.target);
@@ -14,3 +46,4 @@ document.getElementById('contact-info').addEventListener('submit',function(event
     });
     document.getElementById('contact-info').reset();
 });
+
